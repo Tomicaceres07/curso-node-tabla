adaad200b605174c83a0d6561f6cf0296eec4dc2
Tomi <tomicaceres07@gmail.com>
2021-06-22 20:09:55 -0300

    Segundo commit

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -6,14 +6,17 @@ const crearArchivo = async(base = 5, listar = false, hasta = 10) => {
 
     try {
         let salida = '';
+        let consola = '';
+
         for (let i = 1; i <= hasta; i++) {
-            salida += `${base} ${'x'.green} ${i} = ${base*i}\n`;
+            salida += `${base} x ${i} = ${base*i}\n`;
+            consola += `${base} ${'x'.green} ${i} = ${base*i}\n`;
         }
         if (listar) {
             console.log('==============='.green);
             console.log('   Tabla del:'.green, colors.blue(base));
             console.log('==============='.green);
-            console.log(salida);
+            console.log(consola);
         }
         const nombreArchivo = `./salida/tabla-${base}.txt`;
 
